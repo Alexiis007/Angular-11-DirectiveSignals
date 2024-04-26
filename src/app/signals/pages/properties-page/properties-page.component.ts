3b95c600b2d9bf07c9cf8d292555aa8d8125d40b
Alexiis007 <christianalexis0918@gmail.com>
2024-04-26 12:59:18 -0600

    Finalizacion section 24

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interface';
 
 @Component({
@@ -20,24 +20,29 @@ export class PropertiesPageComponent {
 
   public onFielUpdated(field : keyof User, value:string):void{
     //Forma de actualizar solo un dato de la signal<User>
-    // this.user.set({...this.user(),[field]:value})
+    this.user.set({...this.user(),[field]:value})
 
     //Forma 2
-    this.user.update(current => {
-      switch(field){
-      case 'email':
-        current.email = value; 
-        break;
-      case 'last_name':
-        current.last_name = value;
-        break;
-      case 'first_name':
-        current.first_name = value;  
-        break;
-      }
+    // this.user.update(current => {
+    //   switch(field){
+    //   case 'email':
+    //     current.email = value; 
+    //     break;
+    //   case 'last_name':
+    //     current.last_name = value;
+    //     break;
+    //   case 'first_name':
+    //     current.first_name = value;  
+    //     break;
+    //   }
       
-      return current;
-    })
+    //   return current;
+    // });
   }
+
+  public userChangedEffect = effect(()=>{
+    console.log(this.user().first_name+" - "+this.user().email);
+    
+  });
 
 }
